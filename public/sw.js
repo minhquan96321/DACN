f61e335cd8c1800865ed2a80d1f37828bae0eb2f
Your Name <minhquancao0@gmail.com>
2025-01-10 15:53:52 +0700

    fff

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,65 @@
 // public/sw.js
 importScripts("https://cdn.pushalert.co/sw-78204_2.js");
+const CACHE_NAME = "blog-app-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  // Add other assets you want to cache
+];
+
+// Install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Push event
+self.addEventListener("push", (event) => {
+  const options = {
+    body: event.data.text(),
+    icon: "/icons/icon-192x192.png",
+    badge: "/icons/icon-72x72.png",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: "1",
+    },
+    actions: [
+      {
+        action: "explore",
+        title: "Read more",
+        icon: "/icons/checkmark.png",
+      },
+    ],
+  };
+
+  event.waitUntil(self.registration.showNotification("Blog Update", options));
+});
