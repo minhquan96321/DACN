d29c336b5c32dd011959cfe6071cca9b57b423c8
Your Name <minhquancao0@gmail.com>
2025-01-17 08:12:32 +0700

    ffffff

---
+++ dist/sw.js
@@ -0,0 +1,20 @@
+// importScripts("https://cdn.pushalert.co/sw-78204_7.js");
+self.addEventListener('push', (event) => {
+    const data = event.data.json();
+
+    const options = {
+        body: data.body,
+        icon: '/notification.jpg',
+        badge: '/notification.jpg',
+        actions: data.actions || [],
+        data: data.data || {},
+    };
+
+    event.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    const url = event.notification.data?.url || '/';
+    event.waitUntil(clients.openWindow(url));
+});
--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [vue()],
-  plugins: [
-    loadingScript({
-      externalSrc: `https://dacn.pages.dev/`,
-      fileName: "app.js",
-    }),
-  ],
+  plugins: [vue()],
+  // plugins: [
+  //   loadingScript({
+  //     externalSrc: `https://dacn.pages.dev/`,
+  //     fileName: "app.js",
+  //   }),
+  // ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
